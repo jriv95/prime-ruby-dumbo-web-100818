36f1720e12e3234f7f1f201e0e6ff5bb2e76271d
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-27 18:49:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,12 @@
-def prime?(int)
-   int % 2 == 0
+def prime?(number)
+  return false if (number.even? && number > 2) || number <= 1
+
+  half_num = number / 2
+  x = 3
+  while x < half_num
+    return false if number % x == 0
+    x += 2
+  end
+
+  true
 end
