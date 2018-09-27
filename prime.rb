4de9be94fdf8454fb5368ea3f46be5d00b0baa1e
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-27 18:55:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,8 @@ return false if (int.even? && int > 2) || int <= 1
 
 half_int = int / 2
 x = 3
-  while x < half_num
-    return false if number % x == 0
+  while x < half_int
+    return false if int % x == 0
     x += 2
   end
 
